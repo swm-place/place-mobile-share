b94775cd451a28e8bd97e8c289628b6eaba74278
K1A2 <jckim0307@gmail.com>
2023-11-19 10:04:17 +0900

    feat: router place 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HomeComponent from "@/components/HomeComponent.vue";
+import CourseComponent from "@/components/course/CourseComponent.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', name: 'home', component: HomeComponent},
+        {path: '/course', name: 'course', component: CourseComponent}
     ]
 })
 
