fc1737e853838f1aac777ae2a1d86157c1020ea8
K1A2 <jckim0307@gmail.com>
2023-11-19 10:06:13 +0900

    feat: vuetify 프로젝트 추가

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 let app = createApp(App)
+app.use(vuetify)
 app.use(router)
 app.mount('#app')
