8014810b7f55c13188d23e550cf1f25b3ff77e9f
K1A2 <jckim0307@gmail.com>
2023-11-22 19:59:41 +0900

    feat: router 모드 변경

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <router-view />
+  <div id="app">
+<!--    <router-link to="/">main</router-link>-->
+<!--    <router-link to="/course">info</router-link>-->
+    <!-- convert -->
+    <!-- <a href="#/main" class="">main</a> -->
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createWebHistory, createRouter } from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import HomeComponent from "@/components/HomeComponent.vue";
 import CourseComponent from "@/components/course/CourseComponent.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {path: '/', name: 'home', component: HomeComponent},
         {path: '/course', name: 'course', component: CourseComponent}
